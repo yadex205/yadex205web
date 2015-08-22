706acc0b8a776019c88969ef342148febc08a047
Yadex205 <yadex205@outlook.jp>
2015-08-22 10:29:08 +0900

    aliternate ejs with ect

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require("gulp"),
 	plumber = require("gulp-plumber"),
 	notify = require("gulp-notify"),
-	ejs = require("gulp-ejs"),
+	ect = require("gulp-ect"),
 	sass = require("gulp-sass"),
 	server = require("gulp-webserver"),
 	rimraf = require("rimraf");
 	
 var page_files = "src/page/**/*.html",
-	page_watch_files = "src/{layout,page,partial}/**/*.html",
+	watch_page_files = "src/**/*.html",
 	sass_files = "src/sass/**/*.{sass,scss}",
 	image_files = "src/image/**/*.{jpg,png,svg,gif}",
 	script_files = "src/script/**/*.js";
@@ -15,35 +15,34 @@ var page_files = "src/page/**/*.html",
 gulp.task("default", ["build"]);
 
 gulp.task("build", ["html", "sass", "image", "script"]);
-
-gulp.task("test", ["build", "watch"]);
-
-gulp.task("watch", function () {
-	gulp.watch(page_watch_files, ["html"]);
-	gulp.watch(sass_files, ["sass"]);
-	gulp.watch(image_files, ["image"]);
-	gulp.watch(script_files, ["script"]);
+gulp.task("test", ["build"], function () {
 	gulp.src("htdocs")
 		.pipe(server({
 			livereload: true,
 			port: 8080,
 			host: "127.0.0.1"
 		}));
+	gulp.watch(watch_page_files, ["html"]);
+	gulp.watch(sass_files, ["sass"]);
+	gulp.watch(image_files, ["image"]);
+	gulp.watch(script_files, ["script"]);
 });
 
 gulp.task("clean", function (cb) {
 	rimraf("htdocs", cb);
 });
 
-gulp.task("html", function (callback) {
+gulp.task("html", function () {
 	gulp.src(page_files)
 		.pipe(plumber({
 			errorHandler: notify.onError("Error: <%= error.message %>")
 		}))
-		.pipe(ejs())
+		.pipe(ect({
+			ext: ".html"
+		}))
 		.pipe(gulp.dest("htdocs"));
 });
-gulp.task("sass", function (callback) {
+gulp.task("sass", function () {
 	gulp.src(sass_files)
 		.pipe(plumber({
 			errorHandler: notify.onError("Error: <%= error.message %>")
@@ -53,11 +52,11 @@ gulp.task("sass", function (callback) {
 		}))
 		.pipe(gulp.dest("htdocs/css"));
 });
-gulp.task("image", function (callback) {
+gulp.task("image", function () {
 	gulp.src(image_files)
 		.pipe(gulp.dest("htdocs/image"));
 });
-gulp.task("script", function (callback) {
+gulp.task("script", function () {
 	gulp.src(script_files)
 		.pipe(gulp.dest("htdocs/script"));
 });
